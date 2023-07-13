5687e17a246ee756dc0ae4127adf45fb13d7ea72
Lennon <lennon_lxs@hotmail.com>
2023-07-13 17:15:36 -0300

    arruma o sobre

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,7 +12,7 @@ import foto from '../assets/foto.png'
       <h2 className="py-2 text-2xl font-bold text-justify text-orange-500">Desenvolvedor Web Full-Stack</h2>
       <br />
       <p className="py-1 text-sm font-semibold leading-relaxed text-justify text-gray-200">
-        Eu sempre fui apaixonado por tecnologia e por 'desenvolver'/'criar' coisas para melhorar a vida das pessoas, por isso também sou formado em Engenharia Civil, que atuo na área desde 2017, na minha própria empresa a LX Engenharia, depois de anos trabalhando como Engenheiro Civil, decidi por uma transição de carreira e comecei a estudar Desenvolvimento Web FullStack na Trybe, uma escola que ensina a programar, a aprender e a trabalhar.
+        Eu sempre fui apaixonado por tecnologia e por 'desenvolver'/'criar' coisas para melhorar a vida das pessoas, por isso também sou formado em Engenharia Civil, onde atuei desde 2017, na minha própria empresa a LX Engenharia,mas depois de anos trabalhando como Engenheiro Civil, decidi por uma transição de carreira e comecei a estudar Desenvolvimento Web FullStack na Trybe, uma escola que ensina a programar, a aprender e a trabalhar.
       </p>
       <p className="py-1 text-sm font-semibold leading-relaxed text-justify text-gray-200">
         Desde o início de setembro estou cursando a Trybe, aprendendo HTML, Javascript, React, Git, Docker, MySQL, Typescript, Node.js e entre outras tecnologias FullStack.
