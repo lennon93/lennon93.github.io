b90138ee0a88e606df6e8099dfae0ede7999025d
Lennon <lennon_lxs@hotmail.com>
2023-09-15 02:39:41 -0300

    Muda o sobre

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,17 +12,13 @@ import foto from '../assets/foto.png'
       <h2 className="py-2 text-2xl font-bold text-justify text-orange-500">Desenvolvedor Web Full-Stack</h2>
       <br />
       <p className="py-1 text-sm font-semibold leading-relaxed text-justify text-gray-200">
-        Eu sempre fui apaixonado por tecnologia e por 'desenvolver'/'criar' coisas para melhorar a vida das pessoas, por isso também sou formado em Engenharia Civil, onde atuei desde 2017, na minha própria empresa a LX Engenharia,mas depois de anos trabalhando como Engenheiro Civil, decidi por uma transição de carreira e comecei a estudar Desenvolvimento Web FullStack na Trybe, uma escola que ensina a programar, a aprender e a trabalhar.
-      </p>
+      Sempre fui apaixonado por tecnologia e inovação, buscando criar soluções que impactem positivamente a vida das pessoas e o mundo como um todo. Meu grande sonho é desenvolver algo que realmente contribua para o bem do planeta. Esse desejo constante de criar e inovar me levou a uma formação prévia em Engenharia Civil. Durante minha jornada como engenheiro, gerenciei meu próprio escritório e liderei a concepção de diversos projetos.      </p>
       <p className="py-1 text-sm font-semibold leading-relaxed text-justify text-gray-200">
-        Desde o início de setembro estou cursando a Trybe, aprendendo HTML, Javascript, React, Git, Docker, MySQL, Typescript, Node.js e entre outras tecnologias FullStack.
-      </p>
+      Entretanto, no ano passado, decidi realizar uma transição de carreira. Durante esse período, dediquei-me intensamente aos estudos de Desenvolvimento Web Fullstack na Trybe. Este curso, altamente prático e focado nas demandas do mercado, proporcionou uma formação completa. Nele, adquiri conhecimentos desde os fundamentos até as tecnologias mais recentes em Frontend e Backend. Além disso, aprendi a lidar com Bancos de Dados e aprofundei-me em Ciência da Computação, compreendendo algoritmos e desenvolvendo projetos altamente eficientes. Atualmente, estou cursando o módulo de certificação eletiva em Java, onde abordo o desenvolvimento de APIs Rest, utilizando a linguagem de programação Java e o ecossistema Spring.      </p>
       <p className="py-1 text-sm font-semibold leading-relaxed text-justify text-gray-200">
-        Já tendo me formado nos módulos de fundamentos, Frontend e Backend, estou cursando agora o módulo de Ciência da Computação, aprendendo Python, Estrutura de dados e complexidade de algoritmos!
-      </p>
+      Paralelamente a isso, uma vez que não consigo ficar parado, iniciei uma graduação em Tecnologia da Informação na Univesp, uma faculdade pública a distância. Além disso, estou participando de um bootcamp de 3 meses focado em Java mais Angular, como parte de uma bolsa que obtive para estudar na Dio, em parceria com o Santander.      </p>
       <p className="py-1 text-sm font-semibold leading-relaxed text-justify text-gray-200">
-        Ao mesmo tempo, estou no primeiro semestre do curso de Ciência de Dados na faculdade Univesp, onde vou aprender coisas como Aprendizado de máquina, mineração de dados e redes neurais!
-      </p>
+      Estou profundamente entusiasmado em aplicar minha paixão pela tecnologia, minhas habilidades de desenvolvimento e meu conhecimento em projetos desafiadores. Tenho o desejo sincero de contribuir para o sucesso de equipes e organizações inovadoras. Estou totalmente aberto a oportunidades que me permitam crescer profissionalmente e fazer a diferença.      </p>
     </div>
   </div>
   )
